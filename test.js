7861561766b14ec4463556f17f12bbe622d14667
GoldenGoal-Founder <goalgolden2026@gmail.com>
2026-05-26 10:27:00 +0000

    refactor(cleanup): remove obsolete off-chain database utilities and scratch testing scripts

--- test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const routeCode = fs.readFileSync('src/app/api/admin/reset-db/route.js', 'utf8');
-const match = routeCode.match(/INSERT INTO markets\s+\("teamA", "teamB", "matchDate", "pointsReward"\)\s+VALUES\s+([\s\S]+?);/);
-console.log(match[1]);
